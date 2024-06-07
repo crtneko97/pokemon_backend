b8cd79cd30e6e2515aa295ae7a64ecc684a252f5
crtneko97 <simon_kern@hotmail.com>
2024-06-07 10:55:13 +0200

    stats

--- src/main/java/com/example/apiapi/model/Pokemon.java
+++ src/main/java/com/example/apiapi/model/Pokemon.java
@@ -16,13 +16,21 @@ public class Pokemon {
     private String name;
     private int height;
     private int weight;
+    private int base_experience;
+    
+    @ElementCollection
+    private List<Stat> stats;
 
     @ElementCollection
     private List<Ability> abilities;
 
+    @ElementCollection
+    private List<Type> types;
+
     @Embedded
     private Sprites sprites;
 
+    
     // Getters and setters for Pokemon class fields
     public Long getId() {
         return id;
@@ -56,12 +64,28 @@ public class Pokemon {
         this.weight = weight;
     }
 
+    public int getBase_experience() {
+        return base_experience;
+    }
+
+    public void setBase_experience(int base_experience) {
+        this.base_experience = base_experience;
+    }
+
     public List<Ability> getAbilities() {
         return abilities;
     }
 
     public void setAbilities(List<Ability> abilities) {
         this.abilities = abilities;
+    }
+
+    public List<Type> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<Type> types) {
+        this.types = types;
     }
 
     public Sprites getSprites() {
@@ -71,8 +95,51 @@ public class Pokemon {
     public void setSprites(Sprites sprites) {
         this.sprites = sprites;
     }
+    
 
-    // Inner classes with their getters and setters
+    @Embeddable
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Stat {
+        private int base_stat;
+
+        @Embedded
+        private StatDetail stat;
+
+        // Getters and setters...
+
+        public int getBase_stat() {
+            return base_stat;
+        }
+
+        public void setBase_stat(int base_stat) {
+            this.base_stat = base_stat;
+        }
+
+        public StatDetail getStat() {
+            return stat;
+        }
+
+        public void setStat(StatDetail stat) {
+            this.stat = stat;
+        }
+    }
+
+    @Embeddable
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class StatDetail {
+        private String name;
+
+        // Getters and setters...
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 
     @Embeddable
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -80,7 +147,6 @@ public class Pokemon {
         @Embedded
         private AbilityDetail ability;
 
-        // Getters and setters for Ability class fields
         public AbilityDetail getAbility() {
             return ability;
         }
@@ -95,7 +161,35 @@ public class Pokemon {
     public static class AbilityDetail {
         private String name;
 
-        // Getters and setters for AbilityDetail class fields
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @Embeddable
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Type {
+        @Embedded
+        private TypeDetail type;
+
+        public TypeDetail getType() {
+            return type;
+        }
+
+        public void setType(TypeDetail type) {
+            this.type = type;
+        }
+    }
+
+    @Embeddable
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class TypeDetail {
+        private String name;
+
         public String getName() {
             return name;
         }
@@ -110,7 +204,6 @@ public class Pokemon {
     public static class Sprites {
         private String front_default;
 
-        // Getters and setters for Sprites class fields
         public String getFront_default() {
             return front_default;
         }
@@ -120,4 +213,3 @@ public class Pokemon {
         }
     }
 }
-
